093fb66f480eb651703296481bac7d48cf2db57a
David Yurkin <WKDavidY@gmail.com>
2019-01-25 04:47:38 -0800

    publish version 1.3

--- js/main.js
+++ js/main.js
@@ -345,3 +345,55 @@ function appear(theElement, initOpacity, step, speed){
         theElement.style.filter = 'alpha(opacity=' + opacity*100 + ')';
     }, speed);
 }
+
+//swipe detection
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+            changeImg(true, false);
+        } else {
+            /* right swipe */
+            changeImg(false, true);
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */
+            return;
+        } else {
+            /* down swipe */
+            return;
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
